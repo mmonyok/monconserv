cbf6d14a8633c4b5410f3bd5ea5cf0e3d570b208
Ody <melodymonyok@gmail.com>
2022-01-10 18:55:15 -0600

    Added basic Header component and added it to the App.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import Header from './components/Header';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <div id="website">
-        
+        <Header>
+          
+        </Header>
       </div>
     </Router>
   );
---
+++ client/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+//navbar
+
+function Header() {
+  return (
+    <header>
+      {/* insert navbar here with a links array to the different navbar links */}
+      {/* insert children here */}
+    </header>
+  );
+};
+
+export default Header;
