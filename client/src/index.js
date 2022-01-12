00fc85b5e6d4588d0ff5d30fabbd02665f0aff7a
Ody <melodymonyok@gmail.com>
2022-01-12 16:10:44 -0600

    Adding bootstrap.

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 // import reportWebVitals from './reportWebVitals';
 
 // Look more into strict mode: https://reactjs.org/docs/strict-mode.html
