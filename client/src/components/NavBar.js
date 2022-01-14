f62f0a616f739e4042e421a8cb697c8d73b9083b
Ody <melodymonyok@gmail.com>
2022-01-13 19:00:58 -0600

    Added Logo to navbar and made navbar aligned and collapsable.

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,29 +1,31 @@
 import React/* , { useState } */ from 'react';
-import { NavLink } from 'react-router-dom';
+// import { NavLink } from 'react-router-dom';
+import { Container, Navbar, Nav } from 'react-bootstrap';
+import MCS from '../assets/images/MCS.svg';
 
 function NavBar({ links }) {
   return (
-    <nav>
-      <div>
-        <NavLink
-          to="/home">
-          <h4>Monyok Construction Services!</h4>
-        </NavLink>
-
-        <div>
-          <ul>
-            {links.map((link, a, b) => (
-              <li key={a}>
-                <NavLink
-                  key={b}
-                  to={link.href}
-                >{link.name}</NavLink>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
-    </nav>
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">
+          <img
+            alt="MCS Logo"
+            src={MCS}
+            width="100"
+            // height="100"
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          {links.map((link, a) => (
+            <Nav.Link
+              key={a}
+              href={link.href}
+            >{link.name}</Nav.Link>
+          ))}
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
