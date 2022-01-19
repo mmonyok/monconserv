a9235309cf374646b6cf17cd691d31dcd4c13e23
Ody <melodymonyok@gmail.com>
2022-01-18 20:34:10 -0600

    Updated banner to have a smaller height and input a banner title.

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -6,6 +6,7 @@ import block from '../../assets/images/block1.jpg';
 import brick from '../../assets/images/brick1.jpg';
 import excavation from '../../assets/images/excavation1.jpg';
 import demolition from '../../assets/images/demolition1.jpg';
+import '../../assets/styles/Home.css';
 
 const images = [
   {
@@ -33,24 +34,33 @@ const images = [
 export default function Home(/* { images } */) {
   return (
     <section>
-      <Image
-        fluid
-        src={banner} />
-      <Container className='text-center'>
-        <h1>Welcome to Monyok Construction Services!</h1>
-        <p>
-          We are a family owned and operated Concrete and Masonry business. Take a look around to find out a little bit more about us and what we can do for you.
-        </p>
-
+      <div id="bannerWrapper">
+        <Figure id="banner">
+          <Figure.Image
+            fluid
+            src={banner} />
+          <Figure.Caption id="bannerTitle">
+            <h1 id="title">Quality over Quantity</h1>
+          </Figure.Caption>
+        </Figure>
+      </div>
+      <Container>
+        <div className='text-center'>
+          <h1>Welcome to Monyok Construction Services!</h1>
+          <p>
+            We are a family owned and operated Concrete and Masonry business. Take a look around to find out a little bit more about us and what we can do for you.
+          </p>
+        </div>
         <div>
           <h2>WHAT WE DO</h2>
-          <Figure>
+          <Figure className='row row-cols-2'>
             {images.map((image, a) => (
-            <><Figure.Image
+              <div className='col w-50'><Figure.Image
                 key={a}
                 rounded
                 src={image.src}
-                className='border border-dark border-5' /><Figure.Caption>{image.title}</Figure.Caption></>
+                className='border border-dark border-5' />
+                <Figure.Caption className="figureCaption">{image.title}</Figure.Caption></div>
             ))}
           </Figure>
         </div>
