eb9b9c208942765f821e84950dff5d2f44459020
Ody <melodymonyok@gmail.com>
2022-01-20 17:55:09 -0600

    Added the replies subdocument schema for the comments.

--- server/models/Comment.js
+++ server/models/Comment.js
@@ -1,23 +1,25 @@
 const { Schema, model } = require('mongoose');
 const replySchema = require('./Reply');
 
-const commentSchema = new Schema(
-  {
-    content: {
-      type: String,
-      required: true
-    },
-    name: {
-      type: String,
-      required: true,
-    },
-    date: {
-      type: Date,
-      default: Date.now
-    },
-    replies: [replySchema]
-  }
-);
+const commentSchema = new Schema({
+  content: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  approved: {
+    type: Boolean,
+    default: false
+  },
+  replies: [replySchema]
+});
 
 const Comment = model('Comment', commentSchema);
 
---
+++ server/models/Reply.js
@@ -0,0 +1,20 @@
+const { Schema } = require('mongoose');
+
+// This is a subdocument schema, it won't become its own model but we'll use it as the schema for the Comment's `replies` array in Comment.js
+const replySchema = new Schema({
+  content: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true,
+    default: "Joe Monyok"
+  },
+  date:{
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = replySchema;
