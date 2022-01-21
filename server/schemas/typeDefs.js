72be14419ac3840fc50f0577a6c09a245c204430
Ody <melodymonyok@gmail.com>
2022-01-20 19:10:42 -0600

    Added my typeDefs; not sure if they are correct yet.

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,42 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    name: String!
+    email: String!
+  }
+  type Comment {
+    id: ID!
+    content: String!
+    name: String!
+    date: Date!
+    replies: [Reply]
+  }
+  type Reply {
+    id: ID!
+    content: String!
+    name: String!
+    date: Date!
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
+  type Query {
+    getComments: Comment
+  }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    newComment(id: ID,
+      content: String,
+      name: String,
+      date: Date,
+      ): Comment
+    removeComment(id: ID): Comment
+    replyComment(id: ID,
+      content: String,
+      name: String,
+      date: Date): Comment
+  }
+`;
