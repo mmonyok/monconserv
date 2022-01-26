6e76b4c6a7e03aa9a7127463b7e69ceb270bfbb6
Ody <melodymonyok@gmail.com>
2022-01-26 13:15:54 -0600

    Added footer and styling.

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import Header from './components/Header';
+import Footer from './components/Footer';
 import Home from './components/pages/Home';
 import AboutUs from './components/pages/AboutUs';
+import Reviews from './components/pages/Reviews';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -13,8 +15,10 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/home" element={<Home />} />
             <Route path="/about_us" element={<AboutUs />} />
+            <Route path="/reviews" element={<Reviews />} />
           </Routes>
         </Header>
+        <Footer />
       </div>
     </BrowserRouter>
   );
---
+++ client/src/components/Footer.js
@@ -0,0 +1,49 @@
+import React from 'react';
+// import thumbtack from '../assets/images/thumbtack.png';
+import '../assets/styles/Footer.css';
+
+const links = [
+  {
+    name: 'Facebook',
+    class: '',
+    icon: 'fab fa-facebook-square',
+    website: 'https://www.facebook.com/MonyokConstructionServices'
+  },
+  {
+    name: 'Thumbtack',
+    class: 'thumbtack px-2 mx-2',
+    icon: 'fas fa-thumbtack',
+    website: 'https://www.thumbtack.com/mn/jordan/concrete-contractors/monyok-construction-services/service/77640534927090689'
+  },
+  {
+    name: 'LinkedIn',
+    class: '',
+    icon: 'fab fa-linkedin',
+    website: 'https://www.linkedin.com/in/joseph-monyok-79206223/'
+  }
+];
+
+function Footer() {
+  return (
+    <footer className="text-center py-3">
+      <ul className="d-inline-flex">
+        {links.map((link, a, b, c) => (
+          <li
+            key={a}
+            className={`links ${link.class}`}>
+            <a
+              key={b}
+              href={link.website}
+              target="_blank"
+              rel="noreferrer">
+              <i className={`linkIcon me-2 ${link.icon} fa-2x`}>
+              </i>{link.name}</a>
+          </li>
+        ))}
+      </ul>
+      <h5><i class="far fa-copyright"></i> <sup>1995</sup> Monyok Construction Services</h5>
+    </footer>
+  )
+};
+
+export default Footer;
