5693d724100da2026dae39be7370d35d75201228
Ody <melodymonyok@gmail.com>
2022-01-28 17:33:33 -0600

    Updated the home page service cards to be their component.

---
+++ client/src/components/ServiceMain.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { Col, Container, Figure, Row } from 'react-bootstrap';
+import '../assets/styles/ServiceMain.css';
+
+// Main images
+import cement from '../assets/images/cement1.jpg';
+import block from '../assets/images/block1.jpg';
+import brick from '../assets/images/brick1.jpg';
+import excavation from '../assets/images/excavation1.jpg';
+import demolition from '../assets/images/demolition1.jpg';
+
+// This array contains image data, which will make our maps work.
+const images = [
+  {
+    title: "Block Work",
+    src: block,
+  },
+  {
+    title: "Brick Work",
+    src: brick,
+  },
+  {
+    title: "Excavation",
+    src: excavation,
+  },
+  {
+    title: "Demolition",
+    src: demolition,
+  }
+];
+
+export default function ServiceMain() {
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <Figure>
+            <div className="mx-auto w-50 imageWrapper">
+              <Figure.Image
+                rounded
+                src={cement}></Figure.Image>
+              <Figure.Caption
+                className="caption text-end">
+                Cement Work
+              </Figure.Caption>
+            </div>
+          </Figure>
+        </Col>
+      </Row>
+      <Row>
+        {images.map((image, a) => (
+          <Col>
+            <Figure>
+              <div className="imageWrapper">
+                <Figure.Image
+                  key={a}
+                  rounded
+                  src={image.src}></Figure.Image>
+                <Figure.Caption
+                  className="caption text-end">{image.title}</Figure.Caption>
+              </div>
+            </Figure>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  )
+};
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,82 +1,29 @@
 import React from 'react';
-import { Container, Figure } from 'react-bootstrap';
-import banner from '../assets/images/brick_wall.jpg';
-import cement from '../assets/images/cement1.jpg';
-import block from '../assets/images/block1.jpg';
-import brick from '../assets/images/brick1.jpg';
-import excavation from '../assets/images/excavation1.jpg';
-import demolition from '../assets/images/demolition1.jpg';
+import ServiceMain from '../components/ServiceMain';
+import { Container, Image } from 'react-bootstrap';
+import logo from '../assets/images/monconserv.png';
 import '../assets/styles/Home.css';
-
-const images = [
-  {
-    title: "Cement Work",
-    src: cement,
-    class: "col w-100 mx-auto imageWrapper",
-    caption: "homeImageCaptionMain"
-  },
-  {
-    title: "Block Work",
-    src: block,
-    class: "col w-25 imageWrapper",
-    caption: "homeImageCaption"
-  },
-  {
-    title: "Brick Work",
-    src: brick,
-    class: "col w-25 imageWrapper",
-    caption: "homeImageCaption"
-  },
-  {
-    title: "Excavation",
-    src: excavation,
-    class: "col w-25 imageWrapper",
-    caption: "homeImageCaption"
-  },
-  {
-    title: "Demolition",
-    src: demolition,
-    class: "col w-25 imageWrapper",
-    caption: "homeImageCaption"
-  }
-];
 
 export default function Home(props) {
   return (
     <section>
-      <div /* id="bannerWrapper" */>
-        <Figure className="imageWrapper">
-          <Figure.Image
-            fluid
-            src={banner} />
-          <Figure.Caption id="bannerCaption">
-            <h1 id="bannerTitle">Quality over Quantity</h1>
-          </Figure.Caption>
-        </Figure>
-      </div>
       <Container>
         <div className='text-center'>
-          <h1>Welcome to Monyok Construction Services!</h1>
+          <Image
+            fluid
+            src={logo}></Image>
+          <hr />
+          <h1>Welcome!</h1>
           <p>
             We are a family owned and operated Concrete and Masonry business. Take a look around to find out a little bit more about us and what we can do for you.
           </p>
         </div>
         <div>
-          <h2>WHAT WE DO</h2>
-          <p>From block foundations and footings for house foundations to chimney repair. We have you covered for all your concrete and masonry needs.</p>
-          <Figure className='row row-cols-2 justify-content-center'>
-            {images.map((image, a) => (
-              <div className={image.class}><Figure.Image
-                key={a}
-                rounded
-                src={image.src}
-                className='homeImageTitle serviceImage' />
-                <Figure.Caption className={image.caption}>
-                  <h2 className="homeImageTitle">{image.title}</h2></Figure.Caption></div>
-            ))}
-          </Figure>
-        </div>
-      </Container>
+          <h2>WHAT WE <span className="darkSpan">DO</span></h2>
+          <p className="text-center">From block foundations and footings for house foundations to chimney repair. We have you covered for all your concrete and masonry needs.</p>
+          <ServiceMain />
+        </div >
+      </Container >
     </section>
   );
 };
