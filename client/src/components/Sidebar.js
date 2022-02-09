32214c9087e71cf3389db63eb9aee42e1b208ee1
Ody <melodymonyok@gmail.com>
2022-02-09 17:03:09 -0600

    Added a sidebar nav to the services page and fixed the layout for mobile viewing.

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,20 +1,67 @@
 import React from 'react';
-import { Nav } from 'react-bootstrap';
+import concreteIcon from '../assets/images/icons/concreteMixer.png';
+import blockIcon from '../assets/images/icons/concreteBlock.png';
+import brickIcon from '../assets/images/icons/brick.png';
+import excavationIcon from '../assets/images/icons/bobcat.png';
+import demolitionIcon from '../assets/images/icons/jackhammer.png';
+import constructionIcon from '../assets/images/icons/constructionMan.png';
+import { Image, Nav } from 'react-bootstrap';
+import '../assets/styles/Sidebar.css';
+
+const sidebarNavs = [
+  {
+    name: "Main",
+    href: "/services",
+    src: constructionIcon
+  },
+  {
+    name: "Concrete",
+    href: "/concrete",
+    src: concreteIcon
+  },
+  {
+    name: "Block",
+    href: "/block",
+    src: blockIcon
+  },
+  {
+    name: "Brick",
+    href: "/brick",
+    src: brickIcon
+  },
+  {
+    name: "Excavate",
+    href: "/excavation",
+    src: excavationIcon
+  },
+  {
+    name: "Demolish",
+    href: "/demolition",
+    src: demolitionIcon
+  },
+];
 
 export default function Sidebar() {
   return (
-    <>
+    <section id="sidebarWrapper">
       <Nav
         activeKey="/services"
-        as="us"
-        className="flex-column">
-          <Nav.Item as="li">
-            <Nav.Link href="/services">All Services</Nav.Link>
+        as="ul"
+        className="fontBold justify-content-center my-auto"
+        id="sidebar">
+        {sidebarNavs.map((nav, a) => (
+          <Nav.Item className="sidebarNav text-center" as="li">
+            <a href={nav.href}>
+              <Image
+                fluid
+                src={nav.src}
+                className="sidebarIcons mx-auto" />
+              <Nav.Link className="sidebarLinks" href={nav.href}>{nav.name}</Nav.Link>
+            </a>
           </Nav.Item>
-          <Nav.Item as="li">
-            <Nav.Link href="/concrete">Concrete</Nav.Link>
-          </Nav.Item>
+        ))}
       </Nav>
-    </>
+      <hr id="hr" />
+    </section>
   )
 };
--- client/src/pages/Services.js
+++ client/src/pages/Services.js
@@ -1,14 +1,28 @@
-import React from "react";
-import { Container } from "react-bootstrap";
-import { Outlet } from "react-router-dom";
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import ServiceMain from '../components/ServiceMain';
+import Sidebar from '../components/Sidebar';
+import '../assets/styles/ServiceSections.css';
+import '../assets/styles/Sidebar.css';
+
+const currentYear = new Date().getFullYear();
+const year = currentYear - 1995;
 
 export default function AllServices() {
   return (
-    <Container>
-      <h1>I am the all services page!</h1>
-      <section>
-        <Outlet />
-      </section>
+    <Container fluid className="serviceContainer text font">
+      <Sidebar />
+      <main className="d-inline-flex flex-row">
+        <div id="divider" />
+        <section className="serviceSection">
+          <h1 className="my-3 fontBold">Our Services</h1>
+          <div className="text-center">
+            <p>Located out of Jordan, MN & serving the Twin Cities - South/South West Metro Areas.</p>
+            <p>We've been in business {year} years and are fully insured.</p>
+          </div>
+          <ServiceMain />
+        </section>
+      </main>
     </Container>
   )
 };
