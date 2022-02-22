e2d752e1e0e9c98267262aebc54a46fefd42c440
Ody <melodymonyok@gmail.com>
2022-02-21 19:18:03 -0600

    Updated some minor styling issues.

--- client/src/pages/Concrete.js
+++ client/src/pages/Concrete.js
@@ -1,27 +1,34 @@
 import React from 'react';
-import Sidebar from '../components/Sidebar';
 import { Button, Container, ListGroup, OverlayTrigger, Popover } from 'react-bootstrap';
 import Carousel from '../components/Carousel';
-import stoop1 from '../assets/images/concrete/stoop1.jpg';
-import stoop2 from '../assets/images/concrete/stoop2.jpg';
-import stoop3 from '../assets/images/concrete/stoop3.jpg';
+// import stoop1 from '../assets/images/concrete/stoop1.jpg';
+// import stoop2 from '../assets/images/concrete/stoop2.jpg';
+// import stoop3 from '../assets/images/concrete/stoop3.jpg';
 import '../assets/styles/ServiceSections.css';
+
+function importAll(r) {
+  let images = {};
+  r.keys().map((item, index) => {images[item.replace('./', '')] = r(item); });
+  return images;
+}
+
+const images = importAll(require.context('../assets/images/concrete', false, /\.(png|jpe?g|svg)$/));
 
 const data = [
   {
-    image: stoop1,
-    title: "Concrete Stoop",
-    caption: "This is a caption."
+    image: images['apron1.jpg'],
+    title: "Garage Apron Repair",
+    caption: "We cut out the damaged portion then drill holes and insert rebar."
   },
   {
-    image: stoop2,
-    title: "Concrete Stoop",
-    caption: "This is a caption."
+    image: images['apron2.jpg'],
+    title: "Garage Apron Repair",
+    caption: "New concrete is filled in and finished."
   },
   {
-    image: stoop3,
-    title: "Concrete Stoop",
-    caption: "This is a caption."
+    image: images['concretePad.jpg'],
+    title: "Concrete Pad",
+    caption: "We can make a concrete pad to suit any need."
   },
 ];
 
@@ -29,57 +36,51 @@ export default function Concrete() {
   const services1 = ["Footings", "Driveways", "Aprons",
     "Floating Garage Slabs"];
   const services2 = ["Floors", "Patios", "Sidewalks", "Steps"];
-  let path = window.location.pathname;
   return (
-    <Container fluid className="serviceContainer text font">
-      <Sidebar urlPath={path} />
-      <main className="d-inline-flex flex-row">
-        <div className="divider" />
-        <section className="serviceSection text-center">
-          <h1 className="fontBold mb-3 serviceTitle">Concrete Work</h1>
-          <div className="text-center">
-            <p>
-              We are able to do the following concrete projects:
-            </p>
-          </div>
-          <hr />
-          <Container fluid className="listContainer">
-            <ListGroup horizontal="sm" className="mx-auto mb-2 text-center">
-              {services1.map((item) => (
-                <ListGroup.Item className="listItem">{item}</ListGroup.Item>
-              ))}
-            </ListGroup>
-            <ListGroup horizontal="sm" className="text-center mx-auto mb-2">
-              {services2.map((item) => (
-                <ListGroup.Item className="listItem">{item}</ListGroup.Item>
-              ))}
-            </ListGroup>
-          </Container>
-          <OverlayTrigger
-            trigger="click"
-            placement="bottom"
-            overlay={
-              <Popover id="popover-positioned-bottom">
-                <Popover.Header as="h3">Click each option for more info.</Popover.Header>
-                <Popover.Body>
-                  <ListGroup>
-                    <ListGroup.Item>Colored Concrete</ListGroup.Item>
-                    <ListGroup.Item>Exposed Aggregate Concrete</ListGroup.Item>
-                    <ListGroup.Item>Fiber-Mesh Concrete</ListGroup.Item>
-                  </ListGroup>
-                </Popover.Body>
-              </Popover>
-            }
-          >
-            <Button className="mx-auto mt-2" id="optionsBtn">Concrete Options</Button>
-          </OverlayTrigger>
-          <hr />
-          <Container className="mt-3">
-            <Carousel serviceData={data} />
-          </Container>
-        </section>
-        <div className="divider" />
-      </main>
+    <Container className="serviceContainer text">
+      <section className="serviceSection text-center">
+        <h1 className="fontBold mb-3 serviceTitle">Concrete Work</h1>
+        <div className="serviceSubText text-start font">
+          <h4>
+            We are able to do the following concrete projects:
+          </h4>
+        </div>
+        <hr className="hr" />
+        <Container fluid className="listContainer justify-content-center">
+          <ListGroup horizontal="sm" className="my-auto  text-center font me-2 listGroup">
+            {services1.map((item) => (
+              <ListGroup.Item className="listItem text">{item}</ListGroup.Item>
+            ))}
+          </ListGroup>
+          <ListGroup horizontal="sm" className="ms-2 font text-center my-auto listGroup">
+            {services2.map((item) => (
+              <ListGroup.Item className="listItem text">{item}</ListGroup.Item>
+            ))}
+          </ListGroup>
+        </Container>
+        <OverlayTrigger
+          trigger="click"
+          placement="bottom"
+          overlay={
+            <Popover id="popover-positioned-bottom">
+              {/* <Popover.Header as="h3">Click each option for more info.</Popover.Header> */}
+              <Popover.Body className="font text-center popoverBody">
+                <ListGroup>
+                  <ListGroup.Item className="popItem text">Colored Concrete</ListGroup.Item>
+                  <ListGroup.Item className="popItem text">Exposed Aggregate Concrete</ListGroup.Item>
+                  <ListGroup.Item className="popItem text">Fiber-Mesh Concrete</ListGroup.Item>
+                </ListGroup>
+              </Popover.Body>
+            </Popover>
+          }
+        >
+          <Button className="mx-auto mt-2" id="optionsBtn">Concrete Options</Button>
+        </OverlayTrigger>
+        <hr className="hr" />
+        <Container className="mt-3">
+          <Carousel serviceData={data} />
+        </Container>
+      </section>
     </Container>
   )
 };
--- client/src/pages/Services.js
+++ client/src/pages/Services.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import ServiceMain from '../components/ServiceMain';
-import Sidebar from '../components/Sidebar';
+// import Sidebar from '../components/Sidebar';
 import '../assets/styles/ServiceSections.css';
-import '../assets/styles/Sidebar.css';
 
 const currentYear = new Date().getFullYear();
 const year = currentYear - 1995;
 
 export default function AllServices() {
-  let path = window.location.pathname;
   return (
-    <Container fluid className="serviceContainer text font">
-      <Sidebar urlPath={path} />
-      <main className="d-inline-flex flex-row">
-        <div className="divider" />
+      <Container className="serviceContainer">
         <section className="serviceSection">
           <h1 className="my-3 fontBold">Our Services</h1>
           <div className="text-center">
@@ -23,8 +18,6 @@ export default function AllServices() {
           </div>
           <ServiceMain />
         </section>
-        <div className="divider" />
-      </main>
-    </Container>
+      </Container>
   )
 };
