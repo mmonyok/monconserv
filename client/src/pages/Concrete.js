f44ba37a33da8eed6ba8fbe1dc4f78898e018b7f
Ody <melodymonyok@gmail.com>
2022-02-22 00:13:18 -0600

    Added content for concrete service page.

--- client/src/pages/Concrete.js
+++ client/src/pages/Concrete.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { Button, Container, ListGroup, OverlayTrigger, Popover } from 'react-bootstrap';
 import Carousel from '../components/Carousel';
-// import stoop1 from '../assets/images/concrete/stoop1.jpg';
-// import stoop2 from '../assets/images/concrete/stoop2.jpg';
-// import stoop3 from '../assets/images/concrete/stoop3.jpg';
 import '../assets/styles/ServiceSections.css';
 
 function importAll(r) {
@@ -30,6 +27,31 @@ const data = [
     title: "Concrete Pad",
     caption: "We can make a concrete pad to suit any need."
   },
+  {
+    image: images['steps.jpeg'],
+    title: "Concrete Stoop (Steps)",
+    caption: "Whether it is for a new addition or an existing one, we'll make sure it meets your needs."
+  },
+  {
+    image: images['sidewalk.jpg'],
+    title: "Sidewalk",
+    caption: "They can wrap around, be curved, have varying width, etc."
+  },
+  {
+    image: images['patio.jpg'],
+    title: "Fireplace Patio",
+    caption: "This patio is exposed aggregate and has negative space to fit a fire pit."
+  },
+  {
+    image: images['exposedAggregate.jpg'],
+    title: "Exposed Aggregate",
+    caption: "Most concrete pours can utilize exposed aggregate, if desired."
+  },
+  {
+    image: images['footings.jpg'],
+    title: "Concrete Footings",
+    caption: "From additions to garages, we'll ensure you have the base to maintain structural integrity."
+  }
 ];
 
 export default function Concrete() {
@@ -47,12 +69,12 @@ export default function Concrete() {
         </div>
         <hr className="hr" />
         <Container fluid className="listContainer justify-content-center">
-          <ListGroup horizontal="sm" className="my-auto  text-center font me-2 listGroup">
+          <ListGroup horizontal="sm" className="my-auto  text-center font me-2 listGroup" id="groupA">
             {services1.map((item) => (
               <ListGroup.Item className="listItem text">{item}</ListGroup.Item>
             ))}
           </ListGroup>
-          <ListGroup horizontal="sm" className="ms-2 font text-center my-auto listGroup">
+          <ListGroup horizontal="sm" className="ms-2 font text-center my-auto listGroup" id="groupB">
             {services2.map((item) => (
               <ListGroup.Item className="listItem text">{item}</ListGroup.Item>
             ))}
