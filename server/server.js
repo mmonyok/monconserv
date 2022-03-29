65cd1cbf1ffc999a976f4961acd321fd8cba493d
Ody <melodymonyok@gmail.com>
2022-03-28 21:25:58 -0500

    Changing the wildcard back.

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ async function startApolloServer(typeDefs, resolvers) {
     app.use(express.static(path.join(__dirname, '../client/build')));
   }
   
-  app.get('/*', (req, res) => {
+  app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
 
