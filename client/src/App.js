8984ff0d1e7841996504004453450408699191d3
melodymonyok <melody.monyok@compozelabs.com>
2023-04-10 22:28:50 -0500

    Moving app to React 18 and Chakra.

--- client/src/App.js
+++ client/src/App.js
@@ -1,49 +1,62 @@
-import React from 'react';
-import Header from './components/Header';
+import { useEffect } from 'react';
 import Footer from './components/Footer';
+import Header from './components/Header';
 import Layout from './components/Layout';
 import ServiceLayout from './components/ServiceLayout';
-import Home from './pages/Home';
 import AboutUs from './pages/AboutUs';
-import Services from './pages/Services';
-import Reviews from './pages/Reviews';
 import FAQs from './pages/FAQs';
+import Home from './pages/Home';
+import Reviews from './pages/Reviews';
+import Services from './pages/Services';
 // These are our service pages.
-import Concrete from './pages/Concrete';
+import { Box, useDisclosure } from '@chakra-ui/react';
+import { Route, Routes } from 'react-router-dom';
+import './assets/styles/App.css';
+import { WelcomeModal } from './components/WelcomeModal';
+import { Paths } from './config/paths';
 import Block from './pages/Block';
 import Brick from './pages/Brick';
-import Excavation from './pages/Excavation';
+import Concrete from './pages/Concrete';
 import Demolition from './pages/Demolition';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './assets/styles/App.css';
+import Excavation from './pages/Excavation';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <div id="website">
-        <Header>
-          <Routes>
-            <Route element={<Layout />} />
-            <Route index element={<Home />} />
-            <Route path="home" element={<Home />} />
-            <Route path="about_us" element={<AboutUs />} />
-            <Route path="services" element={<ServiceLayout />} >
-              <Route index element={<Services />} />
-              <Route path="concrete" element={<Concrete />} />
-              <Route path="block" element={<Block />} />
-              <Route path="brick" element={<Brick />} />
-              <Route path="excavation" element={<Excavation />} />
-              <Route path="demolition" element={<Demolition />} />
-            </Route>
-            <Route path="reviews" element={<Reviews />} />
-            <Route path="faqs" element={<FAQs />} />
-            <Route path="*" element={<Home />} />
-          </Routes>
-        </Header>
-        <Footer />
-      </div>
-    </BrowserRouter>
-  );
-}
+const App = () => {
+	const { isOpen: isOpen, onOpen: onOpen, onClose: onClose } = useDisclosure();
+
+	useEffect(() => {
+		onOpen();
+	}, []);
+
+	const renderRoutes = () => {
+		return (
+			<>
+				<Route element={<Layout />} />
+				<Route index element={<Home />} />
+				<Route path={Paths.HOME} element={<Home />} />
+				<Route path='about_us' element={<AboutUs />} />
+				<Route path='services' element={<ServiceLayout />}>
+					<Route index element={<Services />} />
+					<Route path='concrete' element={<Concrete />} />
+					<Route path='block' element={<Block />} />
+					<Route path='brick' element={<Brick />} />
+					<Route path='excavation' element={<Excavation />} />
+					<Route path='demolition' element={<Demolition />} />
+				</Route>
+				<Route path='reviews' element={<Reviews />} />
+				<Route path='faqs' element={<FAQs />} />
+				<Route path='*' element={<Home />} />
+			</>
+		);
+	};
+	return (
+		<Box id='website'>
+			<Header>
+				<Routes>{renderRoutes()}</Routes>
+			</Header>
+			<Footer />
+			<WelcomeModal isOpen={isOpen} onClose={onClose} />
+		</Box>
+	);
+};
 
 export default App;
---
+++ client/src/config/theme.ts
@@ -0,0 +1,13 @@
+import { extendTheme } from '@chakra-ui/react';
+
+export const variants = {};
+
+export const theme = extendTheme({
+	colors: {},
+	fonts: {},
+	styles: {},
+	layerStyles: {},
+	components: {},
+});
+
+export default theme;
