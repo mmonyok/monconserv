c8aa6d0b8fff59d9fc393cd091661430b243664e
melodymonyok <melody.monyok@compozelabs.com>
2025-04-16 21:07:56 -0500

    update front page text styling

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,12 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(
+	document.getElementById('root') as HTMLElement
+);
 
 root.render(
 	<BrowserRouter>
--- client/src/pages/MasonryRepairs.tsx
+++ client/src/pages/MasonryRepairs.tsx
@@ -1,13 +1,13 @@
 import '../assets/styles/ServiceSections.css';
 import { ServicePage } from '../components/ServicePage';
-import { masonryRepairsImagesData } from '../data/masonryRepairs';
+import { masonryRepairImagesData } from '../data/masonryRepairImages';
 
 export default function MasonryRepairs() {
 	return (
 		<ServicePage
 			title='Masonry Repairs'
 			subText='We are able to repair the following:'
-			imageData={masonryRepairsImagesData}
+			imageData={masonryRepairImagesData}
 		/>
 	);
 }
